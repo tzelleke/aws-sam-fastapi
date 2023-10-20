1f5be7872837a06176f87aa13cb68cdc74740768
tzelleke <theodros.zelleke@gmail.com>
2023-10-20 19:41:57 +0200

    feat(frontend): AppLink component

---
+++ frontend/src/components/AppLink.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { computed } from 'vue'
+import { RouterLink } from 'vue-router'
+
+const props = defineProps(RouterLink.props)
+const isExternal = computed(() => typeof props.to == 'string' && props.to.startsWith('http'))
+</script>
+
+<template>
+  <a v-if="isExternal" :href="to"><slot /></a>
+  <router-link v-else v-bind="props"><slot /></router-link>
+</template>
